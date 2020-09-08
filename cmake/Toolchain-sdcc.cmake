05854f945ab1dd88191ee9673884a974bebfda73
Boris Bulanek <budovatel@gmail.com>
2020-09-08 13:43:11 +0200

    linux

--- cmake/Toolchain-sdcc.cmake
+++ cmake/Toolchain-sdcc.cmake
@@ -8,18 +8,24 @@ set(BIN_SUFFIX)
 set (SDCC_HOME "/usr" )
 if (WIN32)
 	set(SDCC_HOME "C:/Program\ Files/SDCC")
+    set(BIN_SUFFIX ".exe")
 endif()
-
 
 set(SDCC_BIN_DIR ${SDCC_HOME}/bin )
 set(SDCC_INC_DIR ${SDCC_HOME}/include)
 set(SDCC_LIB_DIR ${SDCC_HOME}/lib)
 
+if (NOT WIN32)
+set(SDCC_BIN_DIR ${SDCC_HOME}/bin )
+set(SDCC_INC_DIR ${SDCC_HOME}/share/sdcc/include)
+set(SDCC_LIB_DIR ${SDCC_HOME}/share/sdcc/lib)
+endif()
+
 
 SET(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-SET(CMAKE_C_COMPILER ${SDCC_BIN_DIR}/sdcc.exe )
-SET(CMAKE_ASM_COMPILER ${SDCC_BIN_DIR}/sdcc.exe )
+SET(CMAKE_C_COMPILER ${SDCC_BIN_DIR}/sdcc${BIN_SUFFIX} )
+SET(CMAKE_ASM_COMPILER ${SDCC_BIN_DIR}/sdcc${BIN_SUFFIX} )
 
 set(CMAKE_C_COMPILER_WORKS 1)
 
